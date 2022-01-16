45e565fa16c4937e398dd150536c13c3e5f6aab4
Attila Fodor <attila.fodor.89@gmail.com>
2022-01-16 09:43:49 +0000

    Add TSDF method

--- main.py
+++ main.py
@@ -72,8 +72,52 @@ def align_scanner():
     vis.vis.run()
 
 
+def tsdf_integration():
+    """Open3d supports simple integration with Truncated Signed Distance Field depth map fusion,
+    so trying it was logical. Unfortunately it doesn't perform very well,
+    due to depth map noise around the base of the cup."""
+
+    pose_data = np.genfromtxt('/home/attila/Datasets/Remy/scanner.log', delimiter=' ', usecols=range(2, 8))
+    rot_mat = pose_data[:, 3:]
+    pose_mat = pose_data[:, 0:3]
+
+    volume = o3d.pipelines.integration.ScalableTSDFVolume(
+        voxel_length=1.0 / 512.0,
+        sdf_trunc=0.4,
+        color_type=o3d.pipelines.integration.TSDFVolumeColorType.RGB8)
+
+    cam_calib = o3d.camera.PinholeCameraIntrinsic(width=640, height=480,
+                                                  fx=613.688, fy=614.261,
+                                                  cx=323.035, cy=242.229)
+
+    for i in range(30):
+        t_mtx = np.identity(4)
+        t_mtx[:3, :3] = o3d.geometry.get_rotation_matrix_from_axis_angle(rot_mat[i, :])
+        t_mtx[:3, 3] = pose_mat[i, :]
+
+        color = cv2.imread(f'/home/attila/Datasets/Remy/rgb-{str(i).zfill(5)}.png', cv2.IMREAD_COLOR)
+        depth = cv2.imread(f'/home/attila/Datasets/Remy/depth-{str(i).zfill(5)}.png', cv2.IMREAD_UNCHANGED)
+        image = o3d.geometry.Image(color)
+        depth_map = o3d.geometry.Image(depth)
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            image, depth_map, depth_trunc=0.6, convert_rgb_to_intensity=False)
+        volume.integrate(
+            rgbd,
+            o3d.camera.PinholeCameraIntrinsic(
+                o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault),
+            np.linalg.inv(t_mtx))
+
+    pcd = volume.extract_point_cloud()
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(pcd, reset_bounding_box=True)
+
+    vis.run()
+
+
 def main():
-    align_scanner()
+    #align_scanner()
+    tsdf_integration()
 
 
 if __name__ == '__main__':
