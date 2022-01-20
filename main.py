1dac783eed9c44de28db7b14e5d663d970258db1
Attila Fodor <attila.fodor.89@gmail.com>
2022-01-20 22:44:42 +0000

    Refactor, add more docstring

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ from scipy.sparse import lil_matrix
 from tqdm import tqdm
 
 import numpy.typing as npt
-from typing import List
+from typing import List, Tuple
 
 
 class PointCloudAligner(object):
@@ -263,35 +263,22 @@ def color_icp():
     vis.run()
 
 
-def refine_camera_poses(rot_mat, pose_mat, color_img_list, cam_calib):
-    """
-    Use bundle adjustment to refine the initial camera pose estimates. Bunde adjustment requires initial estimates of
-     the independent variables, which are the
-       - 6DOF Camera position estimates and calibration parameters
-       - 3D positions of the mappoints
-
-     - Observations of the mappoints as keypoints in images
-
-    To obtain an initial estimate of the mappoints, keypoints are detected in the images and matched by descriptor
-    values. The matches are then merged to create a list of points observed by a list of keypoints each. For each
-    observation the mappoint position is calculated using the depth information and is averaged (excluding 0.0 values
-    meaning depth information is not present).
-
-    In the bundle adjustment stage, mappoints are projected onto the images that observed that given mappoint.
-    A residual value is calculated for each observation by subtracting the projection coordinates from
-    the measured (keypoint) coordinates. The residual values are stored in a sparse matrix, for which the Jacobians
-    (first order partial derivatives) are calculated. We use Scipy's least squares optimisation function to optimize
-    the inputs, the camera parameters and the mappoint locations.
-
-    :param rot_mat:
-    :param pose_mat:
-    :param color_img_list:
-    :param cam_calib:
-    :return:
+class KeypointTracker(object):
     """
 
+    """
+    def __init__(self, ) -> None:
 
-    def track_keypoints(img_list):
+        self.mappoints = np.empty(shape=(0, 3))
+        self.observations = np.empty(shape=(0, 4))
+
+    @staticmethod
+    def _track_keypoints(img_list: List[str]) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        """
+
+        :param img_list:
+        :return:
+        """
         # Generate key points and descriptors with ORB for all images and store in a list of dictionaries
         length = len(img_list)
         detector = cv2.ORB_create()
@@ -302,14 +289,15 @@ def refine_camera_poses(rot_mat, pose_mat, color_img_list, cam_calib):
         kps_ref = np.empty(shape=(0, 2), dtype=np.float32)
 
         # Read first image
-        img_ref = cv2.imread(color_img_list[0], cv2.IMREAD_COLOR)
+        img_ref = cv2.imread(img_list[0], cv2.IMREAD_COLOR)
 
-        #for img_no in tqdm(range(1, length), desc='Keypoint extraction'):
-        for ref_img_no, img_path in enumerate(tqdm(img_list[1:], total=length-1, desc='Keypoint extraction'), start=0):
+        for ref_img_no, img_path in enumerate(tqdm(img_list[1:], total=length - 1,
+                                                   desc='Keypoint extraction'),
+                                              start=0):
             img_track = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
             # If the number of tracked keypoints is under the set threshold,
-            # generate new ones and append to existing tracked keypoints
+            # generate new ones and append to the existing tracked keypoints
             if kps_ref.shape[0] < min_n_features:
                 kps_new = detector.detect(image=img_ref, mask=None)
                 kps_new = np.array([x.pt for x in kps_new], dtype=np.float32)
@@ -333,12 +321,11 @@ def refine_camera_poses(rot_mat, pose_mat, color_img_list, cam_calib):
 
             """
             At this point a VO would be looking for the Essential/Fundamental matrix and recover the pose change,
-            but we already have that approximate information, so skip that step and log the observations and mappoints
+            but we already have that approximate information, so skip this step and log the observations and mappoints.
             """
 
             # For each keypoint find the mappoint if it already has one associated with it when it was tracked before,
             # or create a new
-
             for kp_ref, kp_track in zip(kps_ref, kps_track):
                 mappoint_id = observations[np.where((observations[:, 1] == ref_img_no) *
                                                     (observations[:, 2] == kp_ref[0]) *
@@ -357,29 +344,60 @@ def refine_camera_poses(rot_mat, pose_mat, color_img_list, cam_calib):
                     # Add a new mappoint and the two observations
                     observations = np.vstack([observations,
                                               np.array([mappoints.shape[0], ref_img_no, kp_ref[0], kp_ref[1]], ndmin=2),
-                                              np.array([mappoints.shape[0], ref_img_no + 1, kp_track[0], kp_track[1]], ndmin=2)
+                                              np.array([mappoints.shape[0], ref_img_no + 1, kp_track[0], kp_track[1]],
+                                                       ndmin=2)
                                               ])
 
                     mappoints = np.vstack([mappoints, np.zeros(shape=(1, 3))])
 
             observations = np.unique(np.array(sorted(observations, key=lambda x: (x[0]))), axis=0)
+
+            # Step the references
             img_ref = img_track
             kps_ref = kps_track
 
         return observations, mappoints
 
-    def match_keypoints(keypoints, kp_starts, show_matches=False):
+    @staticmethod
+    def _extract_keypoints(img_list):
+        # Generate key points and descriptors with ORB for all images and store in a list of dictionaries
+        length = len(img_list)
+        orb = cv2.ORB_create()
+
+        # features = [{'key_points': [],
+        #             'descriptors': np.empty((0, 32))} for i in range(length)]  # Initialise a list of dictionaries
+
+        # im_no, x, y, des
+        keypoints = np.empty(shape=(0, 35))
+        kp_starts = [0]  # Store the first keypoint index for the corresponding image no.
+
+        for i in tqdm(range(0, length), desc='Keypoint extraction'):
+            color = cv2.imread(img_list[i], cv2.IMREAD_COLOR)
+            kp = orb.detect(color, None)
+            kp, des = orb.compute(color, kp)
+
+            # features[i]['key_points'] = list(kp)
+
+            keypoints = np.vstack([keypoints, np.hstack([i * np.ones(shape=(len(kp), 1)),
+                                                         np.array([p.pt for p in kp]),
+                                                         des])])
+            kp_starts.append(kp_starts[-1] + len(kp))
+
+        return keypoints, kp_starts
+
+    @staticmethod
+    def _match_keypoints(keypoints, kp_starts, show_matches=False):
         length = len(kp_starts) - 1
 
         # Match keypoints exhaustively with a brute force matcher
         mappoints = np.empty(shape=(0, 3))  # Store the initial point position estimates
         # Store the observed mappoint id and observing image no with keypoint x, y coordinates
         observations = np.empty(shape=(0, 4))
-        for i in tqdm(range(0, length-10), desc='Keypoint matching'):
-            for j in range(i+1, i+11):
+        for i in tqdm(range(0, length - 10), desc='Keypoint matching'):
+            for j in range(i + 1, i + 11):
                 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-                matches = bf.match(queryDescriptors=keypoints[kp_starts[i]: kp_starts[i+1], 3:].astype(np.uint8),
-                                   trainDescriptors=keypoints[kp_starts[j]: kp_starts[j+1], 3:].astype(np.uint8))
+                matches = bf.match(queryDescriptors=keypoints[kp_starts[i]: kp_starts[i + 1], 3:].astype(np.uint8),
+                                   trainDescriptors=keypoints[kp_starts[j]: kp_starts[j + 1], 3:].astype(np.uint8))
                 matches = sorted(matches, key=lambda x: x.distance)
 
                 for match in matches[:50]:
@@ -387,21 +405,22 @@ def refine_camera_poses(rot_mat, pose_mat, color_img_list, cam_calib):
                     mappoints = np.vstack([mappoints, np.zeros(shape=(1, 3))])
                     # Add two observations
                     observations = np.vstack([observations,
-                                              np.hstack([mappoints.shape[0]-1,  # Mappoint id
-                                                         np.array([i]),         # Reference image id
-                                                         keypoints[kp_starts[i]+match.queryIdx][1:3]]),      # x, y
-                                              np.hstack([mappoints.shape[0]-1,  # Same mappoint id
-                                                         np.array([j]),         # Compared to image id
+                                              np.hstack([mappoints.shape[0] - 1,  # Mappoint id
+                                                         np.array([i]),  # Reference image id
+                                                         keypoints[kp_starts[i] + match.queryIdx][1:3]]),  # x, y
+                                              np.hstack([mappoints.shape[0] - 1,  # Same mappoint id
+                                                         np.array([j]),  # Compared to image id
                                                          keypoints[kp_starts[j] + match.trainIdx][1:3]])])  # x, y
 
-        #observations = np.array(sorted(observations, key=lambda x: (x[0])))  # Key: mappoint id
+        # observations = np.array(sorted(observations, key=lambda x: (x[0])))  # Key: mappoint id
         observations = np.array(sorted(observations, key=lambda x: (x[1], x[2], x[3])))
         return observations, mappoints
 
-    def triangulate_mappoints(observations, cam_calib, mappoints, pose_mat, rot_mat):
+    @staticmethod
+    def _triangulate_mappoints(observations, cam_calib, mappoints, pose_mat, rot_mat):
 
         for mappoint in tqdm(np.unique(observations[:, 0]), desc='Triangulating mappoints'):
-            position = np.empty(shape=(0,3))
+            position = np.empty(shape=(0, 3))
             for obs1, obs2 in combinations(observations[observations[:, 0] == mappoint], r=2):
                 t_mtx = np.zeros(shape=(3, 4))
                 t_mtx[:3, :3] = o3d.geometry.get_rotation_matrix_from_axis_angle(rot_mat[int(obs1[1])])
@@ -424,8 +443,46 @@ def refine_camera_poses(rot_mat, pose_mat, color_img_list, cam_calib):
 
         return mappoints
 
+    def compute_points(self, pose_mat: npt.NDArray[np.float64], rot_mat: npt.NDArray[np.float64],
+                       color_img_list: List[str], cam_calib: o3d.camera.PinholeCameraIntrinsic, method: str,
+                       visualise: bool = False) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        """
 
-    def jacobian_sparsity(observations):
+        :param pose_mat:
+        :param rot_mat:
+        :param color_img_list:
+        :param cam_calib:
+        :param method:
+        :param visualise:
+        :return:
+        """
+        # Extract keypoints
+        if method == 'local_tracking':
+            _observations, _mappoints = self._track_keypoints(img_list=color_img_list)
+        elif method == 'global_matching':
+            _keypoints, _kp_starts = self._extract_keypoints(img_list=color_img_list)
+            _observations, _mappoints = self._match_keypoints(keypoints=_keypoints, kp_starts=_kp_starts)
+        else:
+            raise NotImplementedError('Method not implemented. Please choose either \'local_tracking\' or '
+                                      '\'global_matching\'')
+
+        # Triangulate mappoints
+        _mappoints = self._triangulate_mappoints(observations=_observations, cam_calib=cam_calib, mappoints=_mappoints,
+                                          pose_mat=pose_mat, rot_mat=rot_mat)
+
+        self.mappoints = _mappoints
+        self.observations = _observations
+
+        return self.observations, self.mappoints
+
+
+class BundleAdjustment(object):
+    def __init__(self):
+        self.pose_mat = np.empty(shape=(0, 3))
+        self.rot_mat = np.empty(shape=(0, 3))
+
+    @staticmethod
+    def _jacobian_sparsity(observations):
         """
         Calculate which elements of the Jacobian matrix are non-zero
 
@@ -456,7 +513,8 @@ def refine_camera_poses(rot_mat, pose_mat, color_img_list, cam_calib):
 
         return sparsity_matrix
 
-    def project_points(x_params, observations, cam_calib):
+    @staticmethod
+    def _project_points(x_params, observations, cam_calib):
         """This function calculates the residual projection error values for each mappoint-image observation pair
 
         x_params is an (n,) shaped input array containing the parameters to be optimised, which are
@@ -500,33 +558,51 @@ def refine_camera_poses(rot_mat, pose_mat, color_img_list, cam_calib):
         ret = (projected_points - measured_points).ravel()
         return ret
 
-    # Extract keypoints
-    observations, mappoints = track_keypoints(img_list=color_img_list)
+    def refine_poses(self, pose_mat: npt.NDArray[np.float64], rot_mat: npt.NDArray[np.float64],
+                     cam_calib: o3d.camera.PinholeCameraIntrinsic, mappoints: npt.NDArray[np.float64],
+                     observations: npt.NDArray[np.float64]) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        """
+        Use bundle adjustment to refine the initial camera pose estimates. Bunde adjustment requires initial estimates of
+         the independent variables, which are the
+           - 6DOF Camera position estimates and calibration parameters
+           - 3D positions of the mappoints
 
-    # Match keypoints
-    #observations, mappoints = match_keypoints(keypoints=keypoints, kp_starts=kp_starts)
+         - Observations of the mappoints as keypoints in images
 
-    # Triangulate
-    mappoints = triangulate_mappoints(observations=observations, cam_calib=cam_calib, mappoints=mappoints,
-                                      pose_mat=pose_mat, rot_mat=rot_mat)
+        To obtain an initial estimate of the mappoints, keypoints are detected in the images and matched by descriptor
+        values. The matches are then merged to create a list of points observed by a list of keypoints each. For each
+        observation the mappoint position is calculated using the depth information and is averaged (excluding 0.0 values
+        meaning depth information is not present).
 
-    # Merge mappoints - disabled as it's unclear whether it improves results
-    #observations, mappoints = merge_mappoints(mappoints=mappoints, observations=observations)
+        In the bundle adjustment stage, mappoints are projected onto the images that observed that given mappoint.
+        A residual value is calculated for each observation by subtracting the projection coordinates from
+        the measured (keypoint) coordinates. The residual values are stored in a sparse matrix, for which the Jacobians
+        (first order partial derivatives) are calculated. We use Scipy's least squares optimisation function to optimize
+        the inputs, the camera parameters and the mappoint locations.
 
-    # Calculate jacobian sparsity structure
-    sp_mtx = jacobian_sparsity(observations=observations)
+        :param rot_mat:
+        :param pose_mat:
+        :param color_img_list:
+        :param cam_calib:
+        :return:
+        """
+        # Calculate jacobian sparsity structure to be able to fit the Jacobian matrix in memory
+        sp_mtx = self._jacobian_sparsity(observations=observations)
 
-    # Perform LSE optimisation
-    x_params = np.hstack([mappoints.ravel(), np.hstack([pose_mat, rot_mat]).ravel()])
-    res = least_squares(fun=project_points, x0=x_params, jac_sparsity=sp_mtx, loss='linear', verbose=2,
-                        x_scale='jac', ftol=1e-4, method='trf', max_nfev=1000, jac='2-point',
-                        args=(observations, cam_calib))
+        # Perform LSE optimisation
+        x_params = np.hstack([mappoints.ravel(), np.hstack([pose_mat, rot_mat]).ravel()])
+        res = least_squares(fun=self._project_points, x0=x_params, jac_sparsity=sp_mtx, loss='linear', verbose=2,
+                            x_scale='jac', ftol=1e-4, method='trf', max_nfev=100, jac='2-point',
+                            args=(observations, cam_calib))
 
-    # Parse and return the results
-    new_camera_poses = res.x[mappoints.shape[0] * 3:].reshape(-1, 6)
-    _pose_mat, _rot_mat = new_camera_poses[:, :3], new_camera_poses[:, 3:]
+        # Parse and return the results
+        new_camera_poses = res.x[mappoints.shape[0] * 3:].reshape(-1, 6)
+        _pose_mat, _rot_mat = new_camera_poses[:, :3], new_camera_poses[:, 3:]
 
-    return _pose_mat, _rot_mat
+        self.pose_mat = _pose_mat
+        self.rot_mat = _rot_mat
+
+        return self.pose_mat, self.rot_mat
 
 
 def get_better_depthmaps(imgpairs, baseline):
@@ -577,7 +653,7 @@ def main():
     # Data dir with '/' at the end
     data_path = '/home/attila/Datasets/Remy'
 
-
+    # Change dataset limits or stride if needed
     start = 0
     end = 195
     step = 1
@@ -599,19 +675,21 @@ def main():
                                                   cx=323.035, cy=242.229)
 
     # Perform an initial reconstruction with the provided data and inspect results
-    reconstruct(pose_mat, rot_mat, color_img_list, depth_img_list, cam_calib=cam_calib, visualise=True)
-
-    # Generate segmentation masks for the mug to isolate the reconstruction from the background
-    # seg_mask_list = generate_seg_masks(output_dir=data_path)
-    # reconstruct(pose_mat, rot_mat, color_img_list, depth_img_list, visualise=True)
+    #reconstruct(pose_mat, rot_mat, color_img_list, depth_img_list, cam_calib=cam_calib, visualise=True)
 
     # Process stereo images, create better depth maps and inspect results
     #depth_img_list = get_better_depthmaps(zip(ir1_img_list, ir2_img_list), baseline=0.05)
     #reconstruct(pose_mat, rot_mat, color_img_list, depth_img_list, visualise=True)
 
     # Refine camera poses with ORB features and bundle adjustment
-    new_pose_mat, new_rot_mat = refine_camera_poses(rot_mat=rot_mat, pose_mat=pose_mat,
-                                                    color_img_list=color_img_list, cam_calib=cam_calib)
+    tracker = KeypointTracker()
+    observations, mappoints = tracker.compute_points(pose_mat=pose_mat, rot_mat=rot_mat, color_img_list=color_img_list,
+                                                     cam_calib=cam_calib, method='global_matching', visualise=False)
+
+    ba = BundleAdjustment()
+    new_pose_mat, new_rot_mat = ba.refine_poses(pose_mat=pose_mat, rot_mat=rot_mat, mappoints=mappoints,
+                                                observations=observations, cam_calib=cam_calib)
+
     print(np.linalg.norm(pose_mat - new_pose_mat, axis=1))
     print(np.linalg.norm(rot_mat - new_rot_mat, axis=1))
 
